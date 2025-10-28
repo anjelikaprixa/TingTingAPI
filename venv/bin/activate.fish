1554cb0031ddeb91f0d5c89ef5e883ac6dcb007b
Manish <shresthamanish606@gmail.com>
2025-10-28 14:49:03 +0545

    docs api

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/manish/prixa/docs/venv
+set -gx VIRTUAL_ENV /home/manish/prixa/enterprise/TingTingAPI/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
